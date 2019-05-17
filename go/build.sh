8a5eff25f3f45c6f99e47b23a770ec5abd4a7bca
SinLapis <stradust0001@gmail.com>
2019-05-17 15:19:36 +0800

    修改build 路径

--- go/build.sh
+++ go/build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run -v /root/ndp/NDP-task-courier/bin/:/ntc/bin/ -v /root/ndp/NDP-task-courier/src/:/ntc/src/ -e "CGO_ENABLED=0" ndp-task-courier-builder
+docker run -v /root/ndp/NDP-task-courier/go/bin/:/ntc/bin/ -v /root/ndp/NDP-task-courier/go/src/:/ntc/src/ -e "CGO_ENABLED=0" ndp-task-courier-builder
